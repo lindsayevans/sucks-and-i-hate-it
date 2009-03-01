4eb1a42fceea931b40d176521b2305f1acf01536
Lindsay Evans <linz@linz.id.au>
2009-03-01 13:13:10 +1100

    Adding stub user to applicationcontroller & adding userbar stuff

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+    before_filter :login_from_cookie
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '929b79453bc0d77c5532cd118ea8f935'
@@ -24,6 +26,10 @@ class ApplicationController < ActionController::Base
 	@thing = Thing.new attributes
 	render :template => 'things/new'
 	#render :text => '<pre>'+YAML::dump(@thing)
+    end
+
+    def login_from_cookie
+	@current_user = Person.find :first
     end
 
 end
